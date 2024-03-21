810d4335f6eeb88e4b5dc3b1e3e0c906e7f348da
ghiottoandrea <andreaghiotto9@gmail.com>
2024-03-21 17:48:20 +0100

    Add footer + footer scss

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,13 +1,39 @@
 <script>
-export default{
+export default {
     name: `AppFooter`
 }
 </script>
 
 <template>
+    <footer class="mt-3">
+        <div class="container">
+            <div class="credits">
+
+                <div class="info-links">
+                    <h4>Booland s.r.l.</h4>
+                    <ul class="links">
+                        <li><a href="">Informazioni legali |</a></li>
+                        <li><a href="">Informativa sulla provacy |</a></li>
+                        <li><a href="">Diritto di accesso</a></li>
+                    </ul>
+                </div>
+
+                <div class="info-socials">
+                    <h5>Trovaci anche su</h5>
+                    <div class="social">
+                        <i class="fa-brands fa-x-twitter"></i>
+                        <i class="fa-brands fa-facebook"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                        <i class="fa-brands fa-square-pinterest"></i>
+                        <i class="fa-brands fa-youtube"></i>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+    <!-- /#side_footer -->
 
 </template>
 
-<style>
-
-</style>
+<style></style>
