c529fa42b9ef566b51722f3bbb5385d4fe8f6295
ghiottoandrea <andreaghiotto9@gmail.com>
2024-03-21 17:29:52 +0100

    Add main + main scss

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,41 +1,150 @@
 <script>
-export default{
+export default {
     name: `AppMain`
 }
 </script>
 
 <template>
 
-<main>
-    <div class="container">
-        <div class="row">
+    <main>
+        <div class="container">
+            <div class="row">
 
-            <div class="col-4">
-                <div class="card">
-                    <img src="/images/1.webp" alt="">
+                <div class="col-4">
+                    <div class="card mt-3">
+
+                        <i class="fa-solid fa-heart heart"></i>
+                        <span class="discount">-50%</span>
+                        <span class="sostenibility">Sostenibilità</span>
+
+                        <img class="card_top_front_image" src="/images/1.webp" alt="">
+                        <img class="card_top_back_image" src="/images/1b.webp" alt="">
+
+                        <div class="body-card">
+                            <span>Levi's</span>
+                            <h5>RELAXED FIT TEE UNISEX</h5>
+                            <div>
+                                <span class="red-mark"><b>14,99€</b></span>
+                                <span class="line-throu"> 29,99 €</span>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-            </div>
 
-            <div class="col-4">
-                <div class="card">
-                    <img src="/images/1.webp" alt="">
+                <div class="col-4">
+                    <div class="card mt-3">
+
+                        <i class="fa-solid fa-heart heart"></i>
+                        <span class="discount">-30%</span>
+
+                        <img class="card_top_front_image" src="/images/2.webp" alt="">
+                        <img class="card_top_back_image" src="/images/2b.webp" alt="">
+
+                        <div class="body-card">
+                            <span>Guess</span>
+                            <h5>ROSES TEE</h5>
+                            <div>
+                                <span class="red-mark"><b>20,99€</b></span>
+                                <span class="line-throu"> 29,99 €</span>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-            </div>
 
-            <div class="col-4">
-                <div class="card">
-                    <img src="/images/1.webp" alt="">
+                <div class="col-4">
+                    <div class="card mt-3">
+
+                        <i class="fa-solid fa-heart heart"></i>
+                        <span class="discount">-30%</span>
+
+                        <img class="card_top_front_image" src="/images/3.webp" alt="">
+                        <img class="card_top_back_image" src="/images/3b.webp" alt="">
+
+                        <div class="body-card">
+                            <span>Come zucchero filato</span>
+                            <h5>VOGLIA DI COLORI PASTELLO</h5>
+                            <div>
+                                <span class="red-mark"><b>129,99€</b></span>
+                                <span class="line-throu"> 184,99 €</span>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-            </div>
 
+                <div class="col-4">
+                    <div class="card mt-3">
+
+                        <i class="fa-solid fa-heart heart"></i>
+                        <span class="discount">-50%</span>
+                        <span class="sostenibility">Sostenibilità</span>
+
+                        <img class="card_top_front_image" src="/images/4.webp" alt="">
+                        <img class="card_top_back_image" src="/images/4b.webp" alt="">
+
+                        <div class="body-card">
+                            <span>Levi's</span>
+                            <h5>TEE UNISEX</h5>
+                            <div>
+                                <span class="red-mark"><b>14,99€</b></span>
+                                <span class="line-throu"> 29,99 €</span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-4">
+                    <div class="card mt-3">
+
+                        <i class="fa-solid fa-heart heart"></i>
+
+                        <img class="card_top_front_image" src="/images/5.webp" alt="">
+                        <img class="card_top_back_image" src="/images/5b.webp" alt="">
+
+                        <div class="body-card">
+                            <span>Maya Deluxe</span>
+                            <h5>S TRIPE BODICE</h5>
+                            <div>
+                                <span class="red-mark"><b>99,99€</b></span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-4">
+                    <div class="card mt-3">
+
+                        <i class="fa-solid fa-heart heart"></i>
+                        <span class="sostenibility-line">Sostenibilità</span>
+
+                        <img class="card_top_front_image" src="/images/6.webp" alt="">
+                        <img class="card_top_back_image" src="/images/6b.webp" alt="">
+
+                        <div class="body-card">
+                            <span>Esprit</span>
+                            <h5>MAGLIONE - BLACK</h5>
+                            <div>
+                                <span class="red-mark"><b>29,99€</b></span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+            </div>
         </div>
-    </div>
-</main>
+    </main>
 
 </template>
 
 <style>
-img{
+img {
     max-width: 100%;
 
 }
