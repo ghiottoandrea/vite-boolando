bdb8d9ac590ef21fbb5a3681bf21bc6b27456b4c
ghiottoandrea <andreaghiotto9@gmail.com>
2024-03-22 13:43:44 +0100

    Add dynamic card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,17 @@
 <script>
+import productCard from './productCard.vue';
+import { products } from '../data.js';
+
 export default {
-    name: `AppMain`
+    name: `AppMain`,
+    components: {
+        productCard
+    },
+    data() {
+        return {
+            products
+        }
+    }
 }
 </script>
 
@@ -10,131 +21,7 @@ export default {
         <div class="container">
             <div class="row">
 
-                <div class="col-4">
-                    <div class="card mt-3">
-
-                        <i class="fa-solid fa-heart heart"></i>
-                        <span class="discount">-50%</span>
-                        <span class="sostenibility">Sostenibilità</span>
-
-                        <img class="card_top_front_image" src="/images/1.webp" alt="">
-                        <img class="card_top_back_image" src="/images/1b.webp" alt="">
-
-                        <div class="body-card">
-                            <span>Levi's</span>
-                            <h5>RELAXED FIT TEE UNISEX</h5>
-                            <div>
-                                <span class="red-mark"><b>14,99€</b></span>
-                                <span class="line-throu"> 29,99 €</span>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="col-4">
-                    <div class="card mt-3">
-
-                        <i class="fa-solid fa-heart heart"></i>
-                        <span class="discount">-30%</span>
-
-                        <img class="card_top_front_image" src="/images/2.webp" alt="">
-                        <img class="card_top_back_image" src="/images/2b.webp" alt="">
-
-                        <div class="body-card">
-                            <span>Guess</span>
-                            <h5>ROSES TEE</h5>
-                            <div>
-                                <span class="red-mark"><b>20,99€</b></span>
-                                <span class="line-throu"> 29,99 €</span>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="col-4">
-                    <div class="card mt-3">
-
-                        <i class="fa-solid fa-heart heart"></i>
-                        <span class="discount">-30%</span>
-
-                        <img class="card_top_front_image" src="/images/3.webp" alt="">
-                        <img class="card_top_back_image" src="/images/3b.webp" alt="">
-
-                        <div class="body-card">
-                            <span>Come zucchero filato</span>
-                            <h5>VOGLIA DI COLORI PASTELLO</h5>
-                            <div>
-                                <span class="red-mark"><b>129,99€</b></span>
-                                <span class="line-throu"> 184,99 €</span>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="col-4">
-                    <div class="card mt-3">
-
-                        <i class="fa-solid fa-heart heart"></i>
-                        <span class="discount">-50%</span>
-                        <span class="sostenibility">Sostenibilità</span>
-
-                        <img class="card_top_front_image" src="/images/4.webp" alt="">
-                        <img class="card_top_back_image" src="/images/4b.webp" alt="">
-
-                        <div class="body-card">
-                            <span>Levi's</span>
-                            <h5>TEE UNISEX</h5>
-                            <div>
-                                <span class="red-mark"><b>14,99€</b></span>
-                                <span class="line-throu"> 29,99 €</span>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="col-4">
-                    <div class="card mt-3">
-
-                        <i class="fa-solid fa-heart heart"></i>
-
-                        <img class="card_top_front_image" src="/images/5.webp" alt="">
-                        <img class="card_top_back_image" src="/images/5b.webp" alt="">
-
-                        <div class="body-card">
-                            <span>Maya Deluxe</span>
-                            <h5>S TRIPE BODICE</h5>
-                            <div>
-                                <span class="red-mark"><b>99,99€</b></span>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div class="col-4">
-                    <div class="card mt-3">
-
-                        <i class="fa-solid fa-heart heart"></i>
-                        <span class="sostenibility-line">Sostenibilità</span>
-
-                        <img class="card_top_front_image" src="/images/6.webp" alt="">
-                        <img class="card_top_back_image" src="/images/6b.webp" alt="">
-
-                        <div class="body-card">
-                            <span>Esprit</span>
-                            <h5>MAGLIONE - BLACK</h5>
-                            <div>
-                                <span class="red-mark"><b>29,99€</b></span>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
+                <productCard :product="product" :key="product.id" v-for="product in products" />
 
 
             </div>
---
+++ src/components/productCard.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: 'productCard',
+    props:{
+        product: Object
+    }
+}
+</script>
+
+<template>
+
+    <div class="col-4">
+        <div class="card mt-3">
+
+            <i v-if="product.isInFavorites" class="fa-solid fa-heart heart-red"></i>
+            <i v-else class="fa-solid fa-heart heart"></i>
+
+            <div class="badges">
+                <span v-if="product.discount" class="discount">-{{ product.discount }}%</span>
+                <span v-if="product.sostenibility" class="sostenibility">Sostenibilità</span>
+            </div>
+
+            <img class="card_top_front_image" :src="product.frontImage" alt="">
+            <img class="card_top_back_image" :src="product.backImage" alt="">
+
+            <div class="body-card">
+                <span>{{ product.brand }}</span>
+                <h5>{{product.name}}</h5>
+                <div class="prices">
+                    <div class="red-mark"><b>{{ product.priceDiscounted }}$</b></div>
+                    <div v-if="product.discount" class="line-throu"> {{ product.price }}$</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
+<style></style>
